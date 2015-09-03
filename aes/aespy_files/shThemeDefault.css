500027277c26dd15a0aedbaba2b5a70e66ebac2c
Paul Scheidt <scheidt@gmail.com>
2015-09-03 07:08:30 -0700

    add some more projects

---
+++ aes/aespy_files/shThemeDefault.css
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Brandon Sterne &raquo;   Page not found</title>
+  <link rel="stylesheet" href="http://brandon.sternefamily.net/wp-content/themes/sterno/foundation/css/foundation.css" />
+  <link rel="stylesheet" href="http://brandon.sternefamily.net/wp-content/themes/sterno/style.css" />
+  <link rel="icon" type="image/png" href="http://brandon.sternefamily.net/wp-content/themes/sterno/images/lion.png" />
+  <script src="http://brandon.sternefamily.net/wp-content/themes/sterno/foundation/js/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+
+  <div id="header" class="row">
+    <div class="large-12 columns text-center">
+      <a href="http://brandon.sternefamily.net"><img src="http://brandon.sternefamily.net/wp-content/themes/sterno/images/logo.png" alt="Brandon Sterne" /></a>
+    </div>
+  </div>
+
+  <div id="main" class="row"><!-- main row -->
+
+
+    <div class="large-8 push-2 columns"><!-- middle column -->
+
+      <article id="post-0" class="post error404 no-results not-found">
+	<header class="entry-header">
+	  <h2>Article Not Found</h2>
+	</header>
+
+	<div class="entry-content">
+	  <p>We weren't able to find the article you were searching for.</p>
+	  <form role="search" method="get" id="searchform" action="http://brandon.sternefamily.net/">
+  <div class="row">
+    <div class="large-12 columns">
+      <label for="s">Search Posts</label>
+      <input type="text" id="s" name="s" />
+    </div>
+  </div>
+</form>
+	</div>
+      </article>
+
+    </div><!-- end middle column -->
+
+    <div class="large-2 pull-8 columns"><!-- left column -->
+
+      <div class="section-container vertical-nav" data-section="vertical-nav" data-options="one_up: false;">
+<section><p class="title"><a href="https://github.com/bsterne">Code</a></p></section>
+<section><p class="title"><a href="http://sternefamily.net/photos/">Photos</a></p></section>
+</div>
+      <div class="panel ad"><p><strong><a href="http://www.terryneifinglcsw.com">East Bay Psychotherapist</a></strong><br/>Licensed Clinical Social Worker provides psychotherapy and counseling services for couples and individuals in the East Bay Area.</p></div>
+    </div><!-- end left column -->
+
+    <div class="large-2 columns"><!-- right column -->
+      <div id="sidebar" class="panel ad">
+        <aside id="search-4" class="widget widget_search"><form class="custom"><form role="search" method="get" id="searchform" action="http://brandon.sternefamily.net/">
+  <div class="row">
+    <div class="large-12 columns">
+      <label for="s">Search Posts</label>
+      <input type="text" id="s" name="s" />
+    </div>
+  </div>
+</form>
+</form></aside>		<aside id="recent-posts-3" class="widget widget_recent_entries"><form class="custom">		<label>Recent Posts</label>		<ul>
+					<li>
+				<a href="http://brandon.sternefamily.net/2013/11/simple-local-web-servers/">Simple Local Web servers</a>
+						</li>
+					<li>
+				<a href="http://brandon.sternefamily.net/2013/10/standing-on-the-shoulders-of-giants/">Standing on the Shoulders of Giants</a>
+						</li>
+					<li>
+				<a href="http://brandon.sternefamily.net/2011/03/moving-to-github/">Moving to GitHub</a>
+						</li>
+				</ul>
+		</form></aside><aside id="categories-4" class="widget widget_categories"><form class="custom"><label>Categories</label><select name='cat' id='cat' class='postform' >
+	<option value='-1'>Category</option>
+	<option class="level-0" value="7">Books&nbsp;&nbsp;(1)</option>
+	<option class="level-0" value="9">Games&nbsp;&nbsp;(1)</option>
+	<option class="level-0" value="11">Making&nbsp;&nbsp;(1)</option>
+	<option class="level-0" value="10">Mozilla&nbsp;&nbsp;(9)</option>
+	<option class="level-0" value="15">Open Source&nbsp;&nbsp;(1)</option>
+	<option class="level-0" value="3">Programming&nbsp;&nbsp;(22)</option>
+	<option class="level-0" value="4">Python&nbsp;&nbsp;(13)</option>
+	<option class="level-0" value="6">Security&nbsp;&nbsp;(9)</option>
+	<option class="level-0" value="12">Sports&nbsp;&nbsp;(1)</option>
+	<option class="level-0" value="13">Unix&nbsp;&nbsp;(2)</option>
+	<option class="level-0" value="5">Web Development&nbsp;&nbsp;(9)</option>
+	<option class="level-0" value="8">Web Security&nbsp;&nbsp;(6)</option>
+</select>
+
+<script type='text/javascript'>
+/* <![CDATA[ */
+	var dropdown = document.getElementById("cat");
+	function onCatChange() {
+		if ( dropdown.options[dropdown.selectedIndex].value > 0 ) {
+			location.href = "http://brandon.sternefamily.net/?cat="+dropdown.options[dropdown.selectedIndex].value;
+		}
+	}
+	dropdown.onchange = onCatChange;
+/* ]]> */
+</script>
+
+</form></aside><aside id="short_archives-2" class="widget widget_archive"><form class="custom"><label>Archives</label>		<select name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'> <option value="">Month</option> 	<option value='http://brandon.sternefamily.net/2013/11/'> November 2013 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2013/10/'> October 2013 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2011/03/'> March 2011 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2011/01/'> January 2011 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2010/10/'> October 2010 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2010/09/'> September 2010 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2010/08/'> August 2010 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2010/07/'> July 2010 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2010/05/'> May 2010 &nbsp;(4)</option>
+	<option value='http://brandon.sternefamily.net/2010/04/'> April 2010 &nbsp;(2)</option>
+	<option value='http://brandon.sternefamily.net/2010/01/'> January 2010 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2009/10/'> October 2009 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2009/04/'> April 2009 &nbsp;(2)</option>
+	<option value='http://brandon.sternefamily.net/2008/09/'> September 2008 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2008/06/'> June 2008 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2007/12/'> December 2007 &nbsp;(2)</option>
+	<option value='http://brandon.sternefamily.net/2007/06/'> June 2007 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2007/04/'> April 2007 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2007/03/'> March 2007 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2006/11/'> November 2006 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2006/09/'> September 2006 &nbsp;(2)</option>
+	<option value='http://brandon.sternefamily.net/2006/08/'> August 2006 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2006/04/'> April 2006 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2005/06/'> June 2005 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2005/05/'> May 2005 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2005/03/'> March 2005 &nbsp;(1)</option>
+	<option value='http://brandon.sternefamily.net/2005/02/'> February 2005 &nbsp;(2)</option>
+	<option value='http://brandon.sternefamily.net/2005/01/'> January 2005 &nbsp;(1)</option>
+ </select>
+</form></aside>      </div>
+    </div><!-- right column -->
+
+
+  </div><!-- end main row -->
+
+  <footer class="row page-info">
+    <div class="large-12 columns text-center">
+      <div class="large-4 columns text-center">
+        <p>Site design and hosting by Dynamic Media. Proudly powered by <a href="http://wordpress.org/">Wordpress</a>.</p>
+      </div>
+      <div class="large-4 columns text-center">
+        <p>Copyright &copy; 2005–2015 Brandon Sterne. All rights reserved.</p>
+      </div>
+      <div class="large-4 columns text-center">
+        <p><a href="http://www.goines.net/Poster_art1/poster_26.html">Winged Lion of St. Mark</a> reproduced with permission by David Lance Goines.</p>
+      </div>
+    </div>
+  </footer><!-- end footer row -->
+
+
+  <script>
+  document.write('<script src=http://brandon.sternefamily.net/wp-content/themes/sterno/' +
+  ('__proto__' in {} ? 'foundation/js/vendor/zepto' : 'foundation/js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="http://brandon.sternefamily.net/wp-content/themes/sterno/foundation/js/foundation.min.js"></script>
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
